f94261284c41c82c75f51f47e0e3d86f05196787
assafi <assafi@cs.technion.ac.il>
2013-04-24 12:03:52 +0300

    Finished Runner API + Log4j setup

--- src/main/java/il/ac/technion/runner/Runner.java
+++ src/main/java/il/ac/technion/runner/Runner.java
@@ -8,7 +8,7 @@ package il.ac.technion.runner;
 
 import java.io.File;
 import java.util.List;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.*;
 
 /**
  *  This class is used as a hook for us (the course staff) to run your code using <b>your</b> API.
@@ -20,7 +20,7 @@ import java.util.logging.Logger;
  */
 public class Runner {
 	
-	private static Logger logger = Logger.getLogger(Runner.class.getCanonicalName()); 
+	private static Logger logger = LogManager.getLogger(Runner.class); 
 	
 	/**
 	 * This method build/update a <b>persistent</b> index with data taken from the <code>twitterFeed</code>. 
@@ -47,13 +47,39 @@ public class Runner {
 		//TODO Implement setup
 	}
 	
-	/** Returns a list of <code>k</code> words, ordered by descending frequency. 
-	 *  A word, is any sequence of non-whitespace characters. 
-	 *  For example: "So", "much_fun!","@Software", "#Design", "http://tinyurl.com/dx528mt"  
+	/**
+	 * @return Number of tweets in the TKC index
+	 */
+	public int numTweets() {
+		//TODO Implement numTweets
+		return 0;
+	}
+	
+	/**
+	 * @return Number of distinct users who post tweets in the TKC index 
+	 */
+	public int numTwitters() {
+		//TODO Implement numTwitters
+		return 0;
+	}
+	
+	/**
+	 * A hashtag is defined a word prefixed by the <code>#</code> symbol
+	 * @return Number of distinct hashtags
+	 */
+	public int numHashtags() {
+		//TODO Implement numHashtags
+		return 0;
+	}
+	
+	/** Returns a list of <code>k</code> words, ordered by descending frequency as the main order.
+	 *  Second order (in case of frequency equality) is a ascending lexicographic order of the word without
+	 *  the <code>#</code> or <code>@</code> sign.  
+	 *  A word, is any sequence of non-whitespace characters.   
 	 * @param k The size of the returned list. 
 	 * @throws Exception Is thrown in case of I/O errors or any other errors you feel you can't recover from. 
 	 *  You should include a meaningful error message in the exception.
-	 * @return A list of <code>k</code> words, starting from the most frequent word.
+	 * @return A list of <code>k</code> words, including their respective <code>#</code> or <code>@</code> signs.  
 	 */
 	public List<String> topKTrends(int k) throws Exception {
 		logger.info("Retreiving top "+k+" twitter trends");
